34889946fcfa659150e43d620a5288114b147fae
tamasdomotor94 <tamasdomotor94@gmail.com>
2017-07-21 09:12:55 +0200

    Stratégia tervezési minta elkészítése
    Signed-off-by: tamasdomotor94 <tamasdomotor94@gmail.com>

---
+++ ChartBuilder/ChartBuilder/Scripts/Strategies.js
@@ -0,0 +1,28 @@
+﻿var LibraryStrategy = function () {
+    this.library = "";
+};
+
+LibraryStrategy.prototype = {
+    setStrategy: function (library) {
+        this.library = library;
+    },
+    AddStart: function (posx,posy, text) { //param: position {posx,posy}, text
+        return this.library.AddStart(posx,posy,text);
+    }
+};
+
+var GoJsStrategy = function () {
+    this.AddStart = function (posx, posy, text) {
+        console.log("GoJS");
+    }
+}
+var JSPlumbStrategy = function () {
+    this.AddStart = function (posx,posy, text) {
+        console.log("JSPlumb");
+    }
+}
+var mxGraphStrategy = function () {
+    this.AddStart = function (posx,posy, text) {
+        console.log("mxGraph");
+    }
+}
