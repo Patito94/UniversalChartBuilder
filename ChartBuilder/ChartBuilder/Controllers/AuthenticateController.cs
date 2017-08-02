163650832513c2cb15788861051a98f97c40afdb
fulepfanni <fulepfanni@gmail.com>
2017-08-02 11:33:28 +0200

    http kérés backend felé
    Signed-off-by: fulepfanni <fulepfanni@gmail.com>

---
+++ ChartBuilder/ChartBuilder/Controllers/AuthenticateController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+using ChartBuilder.Helpers;
+
+namespace ChartBuilder.Controllers
+{
+    public class AuthenticateController : Controller
+    {
+        // GET: Authenticate
+        public async System.Threading.Tasks.Task<ActionResult> Index()
+        {
+            //using (var client = new HttpClient())
+            //{
+            //    var uri = new Uri("http://www.google.com/");
+
+            //    var response = await client.GetAsync(uri);
+
+            //    string textResult = await response.Content.ReadAsStringAsync();
+
+            //    ViewData["textResult"] = textResult;
+
+            //    return View();
+            //}
+
+            //using (var client = new HttpClient())
+            //{
+            //    var requestMessage = new HttpRequestMessage(HttpMethod.Post, "http://demo.andoc.net:20080/json/reply/Authenticate");
+            //    requestMessage.Headers.Authorization = new AuthenticationHeaderValue("X-Custom-APIKey", "teszt");
+            //    requestMessage.Headers.Authorization = new AuthenticationHeaderValue("Content-Type", "application/json");
+            //    HttpResponseMessage result = await client.SendAsync(requestMessage);
+            //    ViewData["textResult"] = result.Content.ToString();
+
+            //    return View();
+            //}
+
+
+            //string url = "http://demo.andoc.net:20080/json/reply/Authenticate";
+            string json = "{\"UserName\":\"admin\",\"Password\":\"admin\",\"provider\":\"credentials\",\"State\":\"null\",\"oauth_token\":\"null\",\"oauth_verifier\":\"null\"}";
+
+
+            ConnectHelper cn = new ConnectHelper();
+
+            string result = await cn.SendRequestGetResponse(json);
+
+            ViewData["textResult"] = result;
+
+            return View();
+
+        }
+
+
+
+
+
+    }
+}
---
+++ ChartBuilder/ChartBuilder/Helpers/ConnectHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ChartBuilder.Helpers
+{
+
+
+    public class ConnectHelper
+    {
+
+
+        public async Task<string> SendRequestGetResponse(string postData)
+        {
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://demo.andoc.net:20080/json/reply/Authenticate");
+            httpWebRequest.Headers.Add("X-Custom-APIKey", "teszt");
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "POST";
+            
+
+            using (var requestStream = await httpWebRequest.GetRequestStreamAsync())
+            {
+                byte[] postDataArray = Encoding.UTF8.GetBytes(postData);
+                await requestStream.WriteAsync(postDataArray, 0, postDataArray.Length);
+            }
+
+
+            var postResponse = await httpWebRequest.GetResponseAsync() as HttpWebResponse;
+
+            if (postResponse != null)
+            {
+                var postResponseStream = postResponse.GetResponseStream();
+                var postStreamReader = new StreamReader(postResponseStream);
+
+                string response = await postStreamReader.ReadToEndAsync();
+                return response;
+            }
+            return null;
+        }
+
+
+
+
+    }
+}
